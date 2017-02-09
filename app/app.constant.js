08f1ef098fc06f3c547f3d11016151df418a93d5
Stivali Serna <stivaliserna08@gmail.com>
2017-02-09 21:06:29 +0100

    Change API key and navbar style

--- app/app.constant.js
+++ app/app.constant.js
@@ -3,7 +3,7 @@
   angular
     .module('bookStoreApp')
     .constant('cfg', {
-      apiKey: '0b43b9cd5a3c4f81b07b260f4fd8f888',
+      apiKey: 'e1cbd2feb8ce4ebfb5da55a7dcd3f1d8',
       urlHome: 'https://api.nytimes.com/svc/books/v3/lists/overview?api-key=<%API-KEY%>',
       urlCategory: 'https://api.nytimes.com/svc/books/v3/lists/current/<%CATEGORY-NAME%>.json?api-key=<%API-KEY%>',
       urlAuthor: 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=<%API-KEY%>&author=<%AUTHOR%>',
